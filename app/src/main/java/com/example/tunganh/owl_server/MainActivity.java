25aefbeddc584cf7e0f68cc57d1d14045e96175c
Tùng Anh Nguyễn <tunganh2521999@gmail.com>
2018-11-29 19:20:32 +0700

    edit code

--- app/src/main/java/com/example/tunganh/owl_server/MainActivity.java
+++ app/src/main/java/com/example/tunganh/owl_server/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.tunganh.owl_server;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.android.gms.signin.SignIn;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
     Button bt_dangnhap;
@@ -15,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         bt_dangnhap = findViewById(R.id.bt_dangnhap);
 
         bt_dangnhap.setOnClickListener(new View.OnClickListener() {
